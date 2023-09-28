fbe3ab39626035e3d0c7ca6064d7fa1e8dd96e21
Cyrille Lora Ronco <79576141+cloraronco@users.noreply.github.com>
2023-09-28 13:12:14 +0000

    mlx updated

--- minilibx/mlx_destroy_display.c
+++ minilibx/mlx_destroy_display.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   mlx_destroy_display.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: clora-ro <clora-ro@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/03 13:49:46 by clora-ro          #+#    #+#             */
-/*   Updated: 2021/11/09 17:19:53 by clora-ro         ###   ########lyon.fr   */
+/*   Created: 2020/10/03 18:56:35 by mg                #+#    #+#             */
+/*   Updated: 2020/10/04 01:55:35 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "mlx_int.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int	mlx_destroy_display(t_xvar *xvar)
 {
-	size_t	i;
-
-	if (!s)
-		return (NULL);
-	i = 0;
-	while (i < n)
-	{
-		if (*(unsigned char*)(s + i) == (unsigned char)c)
-			return ((void*)(s + i));
-		i++;
-	}
-	return (NULL);
+	XCloseDisplay(xvar->display);
 }
